e1c6c50bae14d2d0a282de525457ad7f1b34021d
eljana <eljana.zeqiraj@pos.al>
2023-08-29 08:47:47 +0200

    First Commit

---
+++ src/components/Meals.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useGlobalContext } from '../context';
+
+const Meals = () => {
+    const { meals, loading, selectMeal, addToFavorites } = useGlobalContext();
+    if (loading) {
+        return <section className="section">
+            <h4>Loading...</h4>
+        </section>
+    }
+
+    if (meals.length < 1) {
+        return <section className="section">
+            <h4>No meals matched your search term. Please try again.</h4>
+        </section>
+    }
+
+    return <section className="section-center">
+        {meals.map((singleMeal) => {
+            const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+            return <article key={idMeal} className="single-meal" >
+                <img src={image} className="img" onClick={() => selectMeal(idMeal)} alt='meal' rel='noreferrer noopener' />
+                <footer>
+                    <h5>{title}</h5>
+                    <button className='like-btn' onClick={() => addToFavorites(idMeal)}>Click Me</button>
+                </footer>
+            </article>
+        })}
+    </section>
+
+}
+
+export default Meals
